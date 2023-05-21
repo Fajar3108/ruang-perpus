6a2dfb491771aafd170ac5bc82dea0045113b255
Fajar3108 <maulanafajaribrahim@gmail.com>
2023-05-22 06:08:48 +0700

    Deploy

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'John Doe'; // TODO: replace this
+
+const studentId = '123456789'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://646a9f15ee907b5f2fbd07db--coruscating-biscotti-8bb969.netlify.app/client/index.html'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
